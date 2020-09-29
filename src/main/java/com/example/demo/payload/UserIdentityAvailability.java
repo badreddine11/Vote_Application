95a7f19bd8a3b9e91278e36d1037ebdf5501de4a
badreddine abdellaoui <badreddineabdellaoui4@gmail.com>
2020-09-29 20:25:07 +0200

    second part of my application

---
+++ src/main/java/com/example/demo/payload/UserIdentityAvailability.java
@@ -0,0 +1,16 @@
+package com.example.demo.payload;
+public class UserIdentityAvailability {
+    private Boolean available;
+
+    public UserIdentityAvailability(Boolean available) {
+        this.available = available;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
---
+++ src/main/java/com/example/demo/payload/VoteRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.payload;
+import javax.validation.constraints.NotNull;
+
+public class VoteRequest {
+    @NotNull
+    private Long choiceId;
+
+    public Long getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(Long choiceId) {
+        this.choiceId = choiceId;
+    }
+}
