60ab0d941bf901595371bc3a949827db92c43fca
badreddine abdellaoui <badreddineabdellaoui4@gmail.com>
2020-09-29 17:30:39 +0200

    first part of my application

---
+++ src/main/java/com/example/demo/customsecurity/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.example.demo.customsecurity;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
---
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.Role;
+import com.example.demo.model.RoleName;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
